5234fc3f5e00d729dcd632bd11cb5300820a1318
lucas-jorge <lucasajorge@gmail.com>
2023-08-07 09:41:26 -0300

    refatorando

--- procurando-alunos.js
+++ procurando-alunos.js
@@ -1,16 +1,19 @@
 // Objetivo: buscar um aluno e exibir sua média
 
+// ...
+
 const alunos = ["João", "Juliana", "Ana", "Caio"];
 const medias = [10, 8, 7.5, 9];
 
 const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno){
-
     if (listaDeAlunosEMedias[0].includes(aluno)) {
-        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+        const [alunos, medias] = listaDeAlunosEMedias;
 
-        const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+        const indice = alunos.indexOf(aluno);
+
+        const mediaDoAluno = medias[indice];
 
         console.log(`${aluno} tem a média ${mediaDoAluno}.`);
 
